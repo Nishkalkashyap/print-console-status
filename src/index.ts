de54549b306a79647abb5fbe443d5273683e4d32
Nishkal <kashyapnishkal@gmail.com>
2019-06-18 13:33:54 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+export function printConsoleStatus(message: string, status: 'danger' | 'success' | 'warning' | 'info', indent: number = 0): void {
+    let emoji = (status == 'danger') ? '  ❗' : (status == 'success') ? ' ✅ ' : (status == 'warning') ? ' ⚠️ ' : ' ️️💁 ';
+    const color = (status == 'danger') ? chalk.redBright : (status == 'success') ? chalk.greenBright : (status == 'warning') ? chalk.yellowBright : chalk.whiteBright;
+    console.log(color(Array(indent).fill('\s').join('') + `| ${emoji}  | ${message}`));
+}
